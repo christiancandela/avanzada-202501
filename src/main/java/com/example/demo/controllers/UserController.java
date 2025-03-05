14dac757bc32827938f4c962452faf59a79634ea
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-03-05 18:26:50 -0500

    Se implementa servicio de negocio.

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.dtos.UserRegistrationRequest;
 import com.example.demo.dtos.UserResponse;
+import com.example.demo.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     @PostMapping
     public ResponseEntity<UserResponse> create(@Valid @RequestBody UserRegistrationRequest request){
-        var response = new UserResponse(UUID.randomUUID().toString(), request.email(), request.fullName(), request.dateBirth(),request.rol());
+        var response = userService.createUser(request);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
@@ -30,6 +32,8 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> get(@PathVariable("id") String id){
-        return ResponseEntity.notFound().build();
+        return userService.getUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
