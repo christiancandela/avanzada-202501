da1628a8f6b988f3d361e0f61d8e025a598a573a
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-03-05 19:01:04 -0500

    Se personalizan la excepción por validación de campos.

--- src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,29 @@
 package com.example.demo.exceptions;
 
 import com.example.demo.dtos.ErrorResponse;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ValueConflictException.class)
     public ResponseEntity<ErrorResponse> handleValueConflictException(ValueConflictException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse("ERROR",ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorResponse>> handleValueConflictException(MethodArgumentNotValidException ex) {
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                ex.getFieldErrors().stream().map(
+                        e->new ErrorResponse("ERROR",e.getField()+"->"+e.getDefaultMessage())
+                ).toList()
+        );
     }
 }
