c98a219b24c16f1b15587a187e5644fdaa21ae3d
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2025-03-18 16:33:39 -0500

    Se adiciona UserRepository para manejar persistencia en base de datos MongoDB.

--- src/main/java/com/example/demo/services/UserServiceImpl.java
+++ src/main/java/com/example/demo/services/UserServiceImpl.java
@@ -1,38 +1,34 @@
 package com.example.demo.services;
 
-import com.example.demo.domain.User;
 import com.example.demo.dtos.UserRegistrationRequest;
 import com.example.demo.dtos.UserResponse;
 import com.example.demo.exceptions.ValueConflictException;
 import com.example.demo.mappers.UserMapper;
+import com.example.demo.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService{
-    private final Map<String, User> userStore = new ConcurrentHashMap<>();
+    private final UserRepository userRepository;
     private final UserMapper userMapper;
+
 
     @Override
     public UserResponse createUser(UserRegistrationRequest user) {
-        if (userStore.values().stream().anyMatch(u -> u.getEmail().equalsIgnoreCase(user.email()))) {
+        if (userRepository.findUserByEmail(user.email()).isPresent()) {
             throw new ValueConflictException("Email ya registrado");
         }
         var newUser = userMapper.parseOf(user);
-//        newUser.setId(UUID.randomUUID().toString());
-//        newUser.setStatus(UserStatus.REGISTERED);
-        userStore.put(newUser.getId(), newUser);
-        return userMapper.toUserResponse(newUser);
+        return userMapper.toUserResponse(userRepository.save(newUser));
     }
 
     @Override
     public Optional<UserResponse> getUser(String id) {
-        return Optional.ofNullable(userStore.get(id))
+        return userRepository.findById(id)
                 .map(userMapper::toUserResponse);
     }
 }
